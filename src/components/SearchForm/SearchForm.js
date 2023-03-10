370c10f503fc5e8311ef30f8e8d4fbfb5cd634cf
Jan Kędzierski <jkedzierski087@gmail.com>
2023-03-10 16:50:55 +0100

    add search funcionality

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,14 +1,25 @@
 import styles from './SearchForm.module.scss';
 import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button';
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
 
-const searchForm = () => {
+const SearchForm = () => {
+  const [searchCard, setSearchCard] = useState('');
+
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch({ type: 'SEARCH_UPDATE', updateSearchString: searchCard });
+    setSearchCard('');
+  };
   return (
-    <form className={styles.searchForm}>
-      <TextInput>Search...</TextInput>
-      <Button><span className="fa fa-search" /></Button>
+    <form className={styles.searchForm} onSubmit={handleSubmit}>
+      <TextInput value={searchCard} onChange={(e) => setSearchCard(e.target.value)}>...Search</TextInput>
+      <Button type="submit"><span className="fa fa-search" /></Button>
     </form>
   );
 };
 
-export default searchForm;
+export default SearchForm;
